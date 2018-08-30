be8c5b713df81c052451dc633878cd8d1f28a839
guoxin <guoxin@qfpay.com>
2018-08-30 20:34:29 +0800

    登录跳转到用户管理

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -121,7 +121,6 @@
                   bicon.style.display = 'none';
                   bicon.src = `${config.ohost}/mchnt/set_cookie?sessionid=${sid}`;
                 }
-
                 this.$router.push('/main/perm_user_index');
               } else {
                 this.$message.error(data.resperr);
